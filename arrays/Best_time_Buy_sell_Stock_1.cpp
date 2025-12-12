bcbc8d40e274842106bd0a06d75601640b1500d1
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-12-12 08:08:50 +0500

    Implement containsNearbyDuplicate and update array solutions
    Added arrays/contain_duplicate_2.cpp with both brute-force and optimal solutions for checking nearby duplicates. Uncommented and updated the brute-force solution in arrays/Two_Sum.cpp to run with a new test case. Commented out the implementation in arrays/Best_time_Buy_sell_Stock_1.cpp, likely for refactoring or deprecation.

--- arrays/Best_time_Buy_sell_Stock_1.cpp
+++ arrays/Best_time_Buy_sell_Stock_1.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-using namespace std;
-int best_time_to_buy_and_sell_stocks(vector<int> &vec)
-{
-    int min_value = INT_MAX;
-    int max_profit = 0;
-    for (auto i = 0; i < vec.size(); i++){
-        if (min_value > vec[i])
-        {
-            min_value = vec[i];
-        }else if (max_profit < vec[i] - min_value)
-        {
-            max_profit = vec[i] - min_value;
-        }
-    }   
-    return max_profit;
-}
-int main()
-{
+// #include <iostream>
+// #include <vector>
+// #include <climits>
+// using namespace std;
+// int best_time_to_buy_and_sell_stocks(vector<int> &vec)
+// {
+//     int min_value = INT_MAX;
+//     int max_profit = 0;
+//     for (auto i = 0; i < vec.size(); i++){
+//         if (min_value > vec[i])
+//         {
+//             min_value = vec[i];
+//         }else if (max_profit < vec[i] - min_value)
+//         {
+//             max_profit = vec[i] - min_value;
+//         }
+//     }   
+//     return max_profit;
+// }
+// int main()
+// {
 
-    vector<int> vec = {7, 1, 5, 3, 6, 4};
-    int result = best_time_to_buy_and_sell_stocks(vec);
-    cout << result << endl;
-    return 0;
-}
+//     vector<int> vec = {7, 1, 5, 3, 6, 4};
+//     int result = best_time_to_buy_and_sell_stocks(vec);
+//     cout << result << endl;
+//     return 0;
+// }
---
+++ arrays/contain_duplicate_2.cpp
@@ -0,0 +1,45 @@
+
+// TLE --> Time Limit Exceed
+
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// bool contain_duplicate_2(vector<int>& vec, int k){
+//     for(int i = 0; i<vec.size(); i++){
+//         for(int j = i; j<vec.size(); j++){
+//             if (vec[i] == vec[j] && j - i == k)
+//             {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+// int main(){
+//     vector<int> vec = {1,2,3,1,2,3};
+//     int response = contain_duplicate_2(vec,2);
+//     cout<<response<<" ";
+//      return 0;
+// }
+
+
+
+// Optimal Solution 
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> lastSeen;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(lastSeen.count(nums[i]) > 0){
+                if(i - lastSeen[nums[i]] <= k){
+                    return true;
+                }
+            }
+            lastSeen[nums[i]] = i;
+        }
+        return false;
+    }
+};
