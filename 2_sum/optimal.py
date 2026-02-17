0055cd1e4a8504a5b27e8a634ed63cda55c0e1ac
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2026-02-17 09:02:27 +0500

    Refactor twoSum to standalone function and add examples
    Replace the Solution.twoSum method with a standalone twoSum(li, target) function, adapting variable names (nums->li, i->idx, num->val) and keeping the same hash-map algorithm. Add a sample call (result = twoSum([2,7,11,15], 9)), commented print, explanatory notes, and a small enumerate() demonstration to illustrate indexing. This makes the module script-oriented and easier to run directly.

--- 2_sum/optimal.py
+++ 2_sum/optimal.py
@@ -1,13 +1,63 @@
-from typing import List
+# from typing import List
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         hashmap = {}
         
-        for i, num in enumerate(nums):
-            complement = target - num
+#         for i, num in enumerate(nums):
+#             complement = target - num
             
-            if complement in hashmap:
-                return [hashmap[complement], i]
+#             if complement in hashmap:
+#                 return [hashmap[complement], i]
             
-            hashmap[num] = i
+#             hashmap[num] = i
+
+
+from typing import List
+def twoSum(li: List[int], target: int) -> List[int]:
+    hashmap = {}
+    
+    for idx, val in enumerate(li):
+        complement = target - val
+        
+        if complement in hashmap:
+            return [hashmap[complement], idx]
+        
+        hashmap[val] = idx
+
+result = twoSum([2,7,11,15], 9)
+# print(result)
+
+
+
+# Notes 
+"""
+a + b = target 
+
+a = 2 
+b = 7 
+
+2 + 7 = 9 
+b = target - a
+7 = 9 - 2
+7 = 7
+
+2 , 7 == a, b
+ 
+ 
+"""
+
+    
+
+# enumerate() is a built-in Python function that allows you to loop over an iterable (like a list) and get:
+
+# ===> The index.
+# ===> The value.
+
+# at the same time.
+
+li = [1,2,3,4,5]
+# i ==> index
+# l ==> val
+for idx , val in enumerate(li):
+    print("index: ",idx," Value: ", val)
